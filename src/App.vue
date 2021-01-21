75b010dacb5044097accee4d224266009da2fb41
oga0927 <totti0927.oga@gmail.com>
2021-01-21 22:46:17 +0900

    修正

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <LikeHeader></LikeHeader>
+  <h2>{{ number }}</h2>
+  <LikeNumber :number="number"></LikeNumber>
+  <LikeNumber :number="number"></LikeNumber>
+</div>
+</template>
+
+<script> 
+import LikeHeader from "./components/LikeHeader.vue"
+
+  export default {
+    data() {
+      return {
+        number: 10
+      }
+    },
+    components: {
+      LikeHeader
+    }
+  };
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>  
---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+ <p>いいね({{ totalNumber / 2 }})</p>
+ <button @click="increment">+1</button>
+</div>
+</template>
+<script>
+  export default {
+    props: ["totalNumber"],
+    methods: {
+      increment() {
+        this.number += 1;
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
+</style>
