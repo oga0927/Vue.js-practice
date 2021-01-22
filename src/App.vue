3b943960afe8569bb7e5da97f8b2df68b3264813
oga0927 <totti0927.oga@gmail.com>
2021-01-22 16:28:32 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 <div>
   <LikeHeader></LikeHeader>
   <h2>{{ number }}</h2>
-  <LikeNumber :number="number"></LikeNumber>
-  <LikeNumber :number="number"></LikeNumber>
+  <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
+  <LikeNumber :total-number="number"></LikeNumber>
 </div>
 </template>
 
@@ -13,11 +13,16 @@ import LikeHeader from "./components/LikeHeader.vue"
   export default {
     data() {
       return {
-        number: 10
+        number: 14
       }
     },
     components: {
       LikeHeader
+    },
+    methods: {
+      incrementNumber(value) {
+        this.number = value;
+      }
     }
   };
 </script>
--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -1,17 +1,22 @@
 <template>
 <div>
- <p>いいね({{ totalNumber / 2 }})</p>
+ <p>いいね({{ halfNumber }})</p>
  <button @click="increment">+1</button>
 </div>
 </template>
 <script>
   export default {
     props: ["totalNumber"],
+    computed: {
+      halfNumber() {
+        return this.totalNumber / 2
+      }
+    },
     methods: {
       increment() {
-        this.number += 1;
+        this.$emit("my-click", this.totalNumber + 1);
       }
-    }
+    },
   };
 </script>
 
