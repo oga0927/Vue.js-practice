7d9e343c71c858d21e0905c78d7a0be9ceab67af
oga0927 <totti0927.oga@gmail.com>
2021-01-23 10:53:16 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,85 @@
 <template>
-<div>
-  <LikeHeader> 
-    <h3>初めまして</h3>
-  </LikeHeader>
-  <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-  <button @click="currentComponent = 'Home'">Home</button>
-  <button @click="currentComponent = 'About'">About</button>
-  <keep-alive>
-  <component :is="currentComponent"></component>
-  </keep-alive>
   <div>
-    <h2>イベントのフォーム</h2>
-    <label for="title">タイトル</label>
+    <LikeHeader> 
+      <h3>初めまして</h3>
+    </LikeHeader>
+    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+    <component :is="currentComponent"></component>
+    </keep-alive>
+    <div>
+      <h2>イベントのフォーム</h2>
+      <label for="title">タイトル</label>
+      <input 
+        id ="title" 
+        type="text"
+        v-model.lazy="eventData.title"
+      >
+      <pre>{{eventData.title}}</pre>
+      <label for="maxNumber">最大人数</label>
+      <input 
+        id ="maxNumber" 
+        type="number"
+        v-model.number="eventData.maxNumber"
+      >
+      <p>{{ eventData.maxNumber}}</p>
+      <label for="host">主催者</label>
+      <input 
+        id ="host" 
+        type="text"
+        v-model.trim="eventData.host"
+      >
+      <pre>{{eventData.host}}</pre>
+      <label for="detail">イベントの内容</label>
+      <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
+      <p style="white-space: pre;">{{eventData.detail}}</p>
+      <input
+        type="checkbox" 
+        id="isPrivate"
+        v-model="eventData.isPrivate"
+      >
+    <label for="isPrivate">非公開</label>
+    <p>{{eventData.isPrivate}}</p>
+    <p>参加条件</p>
     <input 
-      id ="title" 
-      type="text"
-      v-model.lazy="eventData.title"
+      type="checkbox" 
+      id="10" 
+      value="10代"
+      v-model="eventData.target"
     >
-    <pre>{{eventData.title}}</pre>
-    <label for="maxNumber">最大人数</label>
+    <label for="10">10代</label>
     <input 
-      id ="maxNumber" 
-      type="number"
-      v-model.number="eventData.maxNumber"
+      type="checkbox" 
+      id="20" 
+      value="20代"
+      v-model="eventData.target"
     >
-    <p>{{ typeof eventData.maxNumber}}</p>
-    <label for="host">主催者</label>
+    <label for="20">20代</label>
     <input 
-      id ="host" 
-      type="text"
-      v-model.trim="eventData.host"
+      type="checkbox" 
+      id="30" 
+      value="30代"
+      v-model="eventData.target"
     >
-    <pre>{{eventData.host}}</pre>
+    <label for="30">30代</label>
+    <p>{{eventData.target}}</p>
+    <p>参加費</p>
+    <input
+       type="radio" 
+       id="free" 
+       value="無料" 
+       v-model="eventData.price"
+    >
+    <label for="free">無料</label>
+    <input
+       type="radio" 
+       id="paid" 
+       value="有料" 
+       v-model="eventData.price"
+    >
+    <label for="paid">有料</label>
   </div>
 </div>
 </template>
@@ -47,9 +95,13 @@ import Home from "./components/Home.vue";
         number: 14,
         currentComponent: "Home",
         eventData: {
-          title: "タイトル",
+          title: "",
           maxNumber: 0,
-          host: ""
+          host: "",
+          detail: "",
+          isPrivate: false,
+          target: [],
+          price: "無料"
         }
       };
     },
