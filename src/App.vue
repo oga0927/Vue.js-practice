e6f7e51f8ff9e3afc95d79e2dc5b7d5d16f2f97d
oga-0927 <totti0927.oga@gmail.com>
2021-03-07 21:52:29 +0900

    確認

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,79 @@
 <template>
   <div class="main">
     <button @click="myAnimation = 'slide'">Slide</button>
-    <button @click="myAnimation = 'fade'">fade</button>
+    <button @click="myAnimation = 'fade'">Fade</button>
     <p>{{myAnimation}}</p>
+    <br>
+    <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <transition-group 
+        name="fade"
+        tag="div"
+        >
+        <li
+          style="cursor: pointer;"
+          v-for="(number, index) in numbers"
+          :key="number"
+          @click="remove(index)"
+        >{{ number }}</li>
+      </transition-group>
+    </ul>
+    <br>
     <button @click="show = !show">切り替え</button>
     <br><br>
     <transition
-      ：CSS="false"
+      :css="false"
       @before-enter="beforeEnter"
       @enter="enter"
-      @after-enter="afterEnter"
-      @enter-cancelled="enterCancelled"
-        
-      @before-leave="beforeLeave"
-      @leave="Leave"
-      @after-leave="afterLeave"
-      @leave-cancelled="leaveCancelled"
+      @leave="leave"
     >
-      <div 
-        class="circle" 
+      <div
+        class="circle"
         v-if="show"
       ></div>
     </transition>
     <br>
     <button @click="myComponent = 'ComponentA'">ComponentA</button>
     <button @click="myComponent = 'ComponentB'">ComponentB</button>
-    <transition 
+    <transition
       name="fade"
       mode="out-in"
     >
       <component :is="myComponent"></component>
     </transition>
-    <transition 
-         name="fade" 
-         mode="out-in">
-      <p 
-         v-if="show"
-         key="bye"
-      >いらっしゃいませ</p>
-      <p 
-         v-if="!show" 
-         key="hello"
-      >ありがとうございました</p>
+    <transition
+      name="fade"
+      mode="out-in"
+    >
+      <p
+        v-if="show"
+        key="bye"
+      >さよなら</p>
+      <p
+        v-else
+        key="hello"
+      >こんにちは</p>
     </transition>
-    <transition 
-      enter-active-class="animate__animated animate__rubberBand"
-      leave-active-class="animate__animated animate__fadeInLeft"
+    <transition
+      enter-active-class="animated bounce"
+      leave-active-class="animated shake"
       appear
     >
       <p v-if="show">hello</p>
     </transition>
-    <transition 
-      :name="myAnimation" 
+    <transition
+      :name="myAnimation"
       appear
     >
-      <p v-if="show">bye</p>
+      <p v-show="show">bye</p>
     </transition>
   </div>
 </template>
 
 <script>
-import ComponentA from "./components/ComponentA.vue"
-import ComponentB from "./components/ComponentB.vue"
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
+
 export default {
   components: {
     ComponentA,
@@ -69,129 +81,121 @@ export default {
   },
   data() {
     return {
+      numbers: [0, 1, 2],
+      nextNumber: 3,
       show: true,
-      myAnimation: 'slide',
+      myAnimation: "slide",
       myComponent: "ComponentA"
     };
   },
   methods: {
-    /* eslint-disable */
-    beforEnter(el, done) {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el) {
       // 現れる前
+      el.style.transform = "scale(0)";
     },
-    enter(el,done) {
+    enter(el, done) {
       // 現れるとき
-      let scale =0;
+      let scale = 0;
       const interval = setInterval(() => {
         el.style.transform = `scale(${scale})`;
-        scale += 0.1
-        if ( scale > 1) {
+        scale += 0.1;
+        if (scale > 1) {
           clearInterval(interval);
           done();
         }
-      }, 200);
+      }, 20);
     },
-    afterEnter(el) {// eslint-disable-line
-      // 現れた後
-    },
-    enterCancelled(el) {
-      // 現れるアニメーションがキャンセルされた時
-    },
-    beforLeave(el) {
-      // 消える前
-    },
-    Leave(el,done) {
+    leave(el, done) {
       // 消えるとき
-      let scale =1;
+      let scale = 1;
       const interval = setInterval(() => {
         el.style.transform = `scale(${scale})`;
-        scale -= 0.1
-        if ( scale < 0) {
+        scale -= 0.1;
+        if (scale < 0) {
           clearInterval(interval);
           done();
         }
-      }, 200);
-    },
-    afterLeave(el) {
-      // 現れた後
-    },
-    leaveCancelled(el) {
-      // 消えるアニメーションがキャンセルされた時
-    },
-    /* eslint-enable */
+      }, 20);
+    }
   }
-}
-</script> 
+};
+</script>
 
 <style scoped>
-  .circle {
-    width: 200px;
-    height: 200px;
-    margin: auto;
-    border-radius: 100px;
-    background-color: deeppink;
+.circle {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  border-radius: 100px;
+  background-color: deeppink;
+}
+
+.fade-move {
+  transition: transform 1s;
+}
+.fade-enter {
+  /* 現れる時の最初の状態 */
+  opacity: 0;
+}
+.fade-enter-active {
+  /* 現れる時のトランジションの状態 */
+  transition: opacity 1s;
+}
+.fade-enter-to {
+  /*  現れる時の最後の状態 */
+  opacity: 1;
+}
+.fade-leave {
+  /* 消える時の最初の状態 */
+  opacity: 1;
+}
+.fade-leave-active {
+  /* 消える時のトランジションの状態 */
+  transition: opacity 1s;
+  position: absolute;
+  width: 200px;
+}
+.fade-leave-to {
+  /*  消える時の最後の状態 */
+  opacity: 0;
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-active {
+  animation: slide-in 0.5s;
+  transition: opacity 1s;
+}
+.slide-leave-active {
+  animation: slide-in 0.5s reverse;
+  transition: opacity 1s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
   }
-
-
-
-
-
-  .fade-enter {
-    /* 現れる時の最初の状態 */
-    opacity: 0;
+  to {
+    transform: translateX(0);
   }
-  .fade-enter-active {
-    /* 現れる時のトランジョンの状態 */
-     transition: 0.5s;
-  }
-  .fade-enter-to {
-    /* 現れる最後の状態 */
-    opacity: 1;
+}
 
-  }
-  .fade-leave {
-    /* 消える時の最初の状態 */
-    opacity: 1;
-  }
-  .fade-leave-active {
-    /* 消える時のトランジョンの状態 */
-  transition: opacity 0.5s;
-  }
-  .fade-leave-to {
-    /* 消える時の最後の状態 */
-    opacity: 0;
-  }
-
-  .slide-enter,
-  .slide-leave-to {
-    opacity: 0;
-  }
-
-
-
-  .slide-enter-active {
-    animation: slide-in 0.5s;
-    transition: opacity 1s;
-  }
-
-  .slide-leave-active {
-    animation: slide-in 0.5s reverse;
-    transition: opacity 1s;
-  }
-
-  @keyframes slide-in {
-    from {
-      transform: translateX(100px);
-    }
-    to {
-      transform: translateX(0);
-    }
-  }
-
-  .main {
-    width: 70%;
-    margin: auto;
-    padding: 5rem;
-    text-align: center ;
-  }
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
 </style>
