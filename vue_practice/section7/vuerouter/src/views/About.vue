33b0d89f188b70b57a9f94cda148003b1f5780d6
oga-0927 <totti0927.oga@gmail.com>
2021-03-15 20:33:41 +0900

    name変更

---
+++ vue_practice/section7/vuerouter/src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="clickInfo">コンソールに移動</button><br><br>
+    <button @click="gotoHome">ホームに戻る</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  methods: {
+    clickInfo() {
+      console.log(this.$route);
+      console.log(this.$route.path);
+      console.log(this.$router);
+    },
+    gotoHome() {
+      // $router.push()でページ遷移
+      this.$router.push('/')
+    }
+  }
+}
+</script>
---
+++ vue_practice/section7/vuerouter/src/views/BookList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="book">
+    aaaa
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bool',
+  methods: {
+    enterInfo() {
+      this.$router.push('./About.vue')
+    }
+  }
+}
+</script>
