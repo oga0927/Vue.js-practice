69b70cec3562b73ec468abc45f19212d6ae369ac
oga-0927 <totti0927.oga@gmail.com>
2021-03-15 18:26:35 +0900

    と

--- vue_practice/section7/vueruter/src/App.vue
+++ vue_practice/section7/vueruter/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about" >About</router-link>
     </div>
     <router-view/>
   </div>
@@ -27,6 +27,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: red;
 }
 </style>
