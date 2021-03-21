905f00f563df2c862f8d8e12083b279cb5b4b19f
oga-0927 <totti0927.oga@gmail.com>
2021-03-21 16:08:12 +0900

    vuexインストール

--- vue_practice/section9/vuex/src/App.vue
+++ vue_practice/section9/vuex/src/App.vue
@@ -5,37 +5,28 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-    <button @click="setLogin">ログイン名表示</button>
-    {{ $store.state.count }}
-    <br>
-    <ul>
-      <li v-for="user in visibleUsers" :key="user.id">
-      {{ user.id }} : {{ user.name }} : {{ user.isVisible }}
-      </li>
-    </ul>
-    <br>
-    {{ getUserById }}
   </div>
 </template>
 
-<script>
-import { mapActions } from 'vuex' 
-
-export default {
-  methods:{
-    ...mapActions('auth', ['setLoginUser']),
-    setLogin(){
-      this.setLoginUser({name:'大谷'})
-    }
-  },
-  computed:{
-    visibleUsers(){
-      return this.$store.getters.visibleUsers
-    },
-    getUserById(){
-      return this.$store.getters.getUserById(2)
-    }
-  }  
-  
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
