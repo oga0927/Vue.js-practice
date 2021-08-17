f8d59fb180631996171357ea1ea90686c51dbf9d
OGA <totti0927.oga@gmail.com>
2021-08-17 11:40:18 +0900

    合成関数のモジュール化

--- vue_practice/section10/vue3-test/src/views/FunctionTest.vue
+++ vue_practice/section10/vue3-test/src/views/FunctionTest.vue
@@ -1,10 +1,56 @@
 <template>
-  <div></div>
+  <div>ComositionFunctionTest</div>
+  <div>
+    <p>商品名: {{ item.name }}</p>
+    <p>単価: {{ item.price }}</p>
+    <p>合計: {{ totalPrice }}</p>
+  </div>
+  <div>数量</div>
+  <button @click="decrement">-</button>
+  <button @click="increment">+</button>
 </template>
 
 <script>
-export default {
+// import { reactive, computed } from 'vue'
+import { reactive } from 'vue'
+import useCounter from '@/composables/useCounter'
 
+//script内で合成関数を作成する場合
+// const useCounter = item => {
+//   const increment = () => {
+//     item.amount++
+//   }
+//   const decrement = () => {
+//     item.amount--
+//   }
+  
+//   const totalPrice = computed(() => {
+//     return item.amount * item.price
+//   })
+
+//   return {
+//     increment,
+//     decrement,
+//     totalPrice
+//   }
+// } 
+
+export default {
+  setup() {
+    const item = reactive({
+      name: '商品名',
+      price: '100',
+      amount: '0'
+    })
+
+    const { increment, decrement, totalPrice} = useCounter(item)
+    return {
+      item,
+      increment,
+      decrement,
+      totalPrice
+    }
+  }
 }
 </script>
 
