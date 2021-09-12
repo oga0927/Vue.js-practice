6214ca52e6f2f16af9e0c102b676a23667c8ed01
OGA <otti0927.oga@gmail.com>
2021-09-12 14:08:51 +0900

    vue-test追加

---
+++ vue_practice/vue_test/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="addCount">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    addCount() {
+      // コンポーネントからはcommitでmutationを呼び出す
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
+
